c1f72faafd5fa8b04f8a7b390a21f06a13303306
Roohullah3530 <93561470+Roohullah3530@users.noreply.github.com>
2022-01-17 05:30:09 +0500

    Create ROOHULLAH.py
    apt update 
     apt upgrade
    pkg install python2
    pkg install git
    git clone https://github.com/Roohullah3530/ROOHULLAH.git
    pip2 install requests bs4
    pip2 install futures
    Run script
    cd  ROOHULLAH
    python2 ROOHULLAH.py

---
+++ ROOHULLAH.py
@@ -0,0 +1,40 @@
+<h3><b><i>🐓 Github Statistics :</i></b></h3>
+<a href="https://github.com/ROOHULLAH-RONY"><img title="trophy" src="https://github-profile-trophy.vercel.app/?username=ROOHULLAH-RONY&theme=monokai"></a>
+</p>  
+<p align="center"> 
+ 𝚅𝙸𝚂𝙸𝚃𝙾𝚁𝚂 𝙲𝙾𝚄𝙽𝚃
+ <img src="https://profile-counter.glitch.me/ROOHULLAH-RONY/count.svg" />
+</p>
+
+
+
+
+
+![github stats](https://github-readme-stats.vercel.app/api?username=ROOHULLAH-RONY&show_icons=true&include_all_commits=true&theme=chartreuse-dark&cache_seconds=3200)
+
+<img align="center" src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=ROOHULLAH-RONY&layout=compact&theme=chartreuse-dark" />
+
+
+#Languages and Tools
+</p>
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/html.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/csharp.svg"alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/languages/js.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/misc/cloud.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/misc/datascience.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/services/aws.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/services/npm.svg" alt="Twitter" style="vertical-align:top; margin:4px"> <img src="https://raw.githubusercontent.com/8bithemant/8bithemant/master/svg/dev/tools/bash.svg" alt="Twitter" style="vertical-align:top; margin:4px">
+ </p>
+<p align="center">
+<code><a href="https://www.python.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/python/python-ar21.svg"></a></code>
+<code><a href="https://www.linux.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/linux/linux-ar21.svg"></a></code>
+<code><a href="https://reactjs.org/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"></a></code>
+<code><a href="https://www.docker.com/" target="_blank"><img height="50" src="https://www.vectorlogo.zone/logos/docker/docker-official.svg"></a></code>
+<br/><br/>
+</p>
+
+<p align="center">
+<a href="https://github.com/ROOHULLAH-RONY/HUNTER"><img title="HUNTER" src="https://github-readme-stats.vercel.app/api/pin/?username=ROOHULLAH-RONY&repo=HUNTER&theme=vision-friendly-dark"></a>
+<a href="https://github.com/ROOHULLAH-RONY/BRAVO"><img title="BRAVO" src="https://github-readme-stats.vercel.app/api/pin/?username=ROOHULLAH-RONY&repo=BRAVO&theme=dark"></a>
+<a href="https://github.com/ROOHULLAH-RONY/DRAG-SDJ"><img title="DRAG-SDJ" src="https://github-readme-stats.vercel.app/api/pin/?username=ROOHULLAH-RONY&repo=DRAG-SDJ&theme=vision-friendly-dark"></a>
+<a href="https://github.com/ROOHULLAH-RONY/COBR4"><img title="COBR4" src="https://github-readme-stats.vercel.app/api/pin/?username=ROOHULLAH-RONY&repo=COBR4&theme=tokyonight"></a>
+</p>
+
+
+
